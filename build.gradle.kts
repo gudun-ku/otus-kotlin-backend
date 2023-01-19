a6885a6100125d99d9a0edbb5ba9a50757095409
Белоушкин Александр Николаевич <gudun.ku@gmail.com>
2023-01-19 20:49:13 +0300

    [m1l1] Add initial project and first module

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") apply false
+}
+
+group = "ru.otus.otuskotlin.marketplace"
+version = "0.0.1"
+
+
+repositories {
+    mavenCentral()
+}
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+
+    repositories {
+        mavenCentral()
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
---
+++ m1l1-quickstart/src/main/kotlin/Main.kt
@@ -0,0 +1,5 @@
+package ru.otus.otuskotlin.marketplace.m1l1
+
+fun main() {
+    println("Module 1 says Hello, World!")
+}
